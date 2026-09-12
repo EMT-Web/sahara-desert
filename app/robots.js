f8a8685e2bd583549cb4abba135ecd299872c00e
EMT-Web <info@escortedmoroccotours.com>
2026-09-12 17:36:04 +0100

    Add files via upload

---
+++ app/robots.js
@@ -0,0 +1,15 @@
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.visitsaharadesert.com'
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/studio/', '/_next/static/media/'],
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+  }
+}
+
